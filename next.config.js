bc19788091cfd424c5c5515dd446aad23a6f20df
utkarsh <us5927060@gmail.com>
2026-03-22 20:18:39 +0530

    hardcode api url to test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_API_URL: 'https://snapshotapi-backend-production.up.railway.app',
   },
 }
 
