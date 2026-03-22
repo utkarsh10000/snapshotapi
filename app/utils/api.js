436fdec3a432185c286f4f7a61e10adaa44ed064
utkarsh <us5927060@gmail.com>
2026-03-22 17:37:43 +0530

    fix api url to use environment variable

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:5000'
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000'
 
 // Get token from localStorage
 export const getToken = () => {
