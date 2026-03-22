d45dfe101ecc49ba6809c98b1b320f418423457d
utkarsh <us5927060@gmail.com>
2026-03-22 23:01:22 +0530

    hardcode paddle token

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: 'https://snapshotapi-backend-production.up.railway.app',
-    NEXT_PUBLIC_PADDLE_CLIENT_TOKEN: process.env.NEXT_PUBLIC_PADDLE_CLIENT_TOKEN,
+    NEXT_PUBLIC_PADDLE_CLIENT_TOKEN: 'live_09533c8a8e7c5ebff42d41746bc',
   },
 }
 
